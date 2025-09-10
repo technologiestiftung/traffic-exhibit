48c3897d479860b7a1564fb4a7cb7370d8998496
aeschi <anna.eschenbacher@ts.berlin>
2025-09-10 14:56:12 +0200

    fix: ci workflows build prettier lint

--- apps/web-app/src/App.tsx
+++ apps/web-app/src/App.tsx
@@ -1,14 +1,14 @@
 import { useWebSocket } from "./hooks/useWebSocket";
 
 function App() {
-  const { numbers, stopMotor } = useWebSocket();
+	const { numbers, stopMotor } = useWebSocket();
 
-  return (
-    <div>
-      <h1>Camera Data: {numbers.join(", ")}</h1>
-      <button onClick={stopMotor}>Stop Motor</button>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Camera Data: {numbers.join(", ")}</h1>
+			<button onClick={stopMotor}>Stop Motor</button>
+		</div>
+	);
 }
 
 export default App;
