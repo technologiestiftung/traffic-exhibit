fb6cbc45a99af68caa15e08dc1f6f2a45eb2bbed
Engy El Shenawy <122390215+Engy-ai@users.noreply.github.com>
2026-04-21 14:08:50 +0200

    fix: scaling

--- apps/web-app/src/i18n/translations/de.ts
+++ apps/web-app/src/i18n/translations/de.ts
@@ -16,7 +16,7 @@ export const de = {
 	"match.title.noCloseMatch.subtitle":
 		"Leider konnten wir aktuell keinen passenderen Verkehrs-Mix finden.",
 	"match.subtitle":
-		"basierend auf den Verkehrsdaten der ",
+		"Diese Straßen passen am besten zu deiner Suche – basierend auf Verkehrsdaten der ",
 	"match.subtitle.highlight": "letzten Stunde",
 	"match.subtitle.end": ".",
 	"match.livedata.tooltip":
